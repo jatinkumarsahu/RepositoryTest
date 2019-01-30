c657c8d70d877430b3f0348568e0c3eba75e2835
Jatin.Sahu <jatinkumar.sahu619@gmail.com>
2019-01-31 02:37:10 +0530

    ccc

--- src/com/test/TestMainClass.java
+++ src/com/test/TestMainClass.java
@@ -1,10 +1,13 @@
 package com.test;
 
+import java.util.Calendar;
+
 public class TestMainClass {
 	
 	public static void main(String...ar)
 	{
 		System.out.println("Hellaaaa");
+		System.out.println(Calendar.getInstance().get(Calendar.YEAR));
 	}
 
 }
